5cac03b2d46d37b337cffd17461a4b8be7342a5c
hqgitter <69576378+hqgitter@users.noreply.github.com>
2020-10-07 01:26:00 +0800

    Adding Lei Dice

---
+++ lei.js
@@ -0,0 +1,47 @@
+
+var leistuff = 
+[
+"The floor is lava",
+"Snake boi",
+"Panther man",
+"Dragons united",
+"Tiger heavy",
+"Cranesanity",
+"Phoenix Illusion",
+"Drunkard",
+"f31 train",
+"backturned bull",
+"forward only",
+"extreme solidity",
+"cautious",
+"defensive",
+"whiff only",
+"sleepy lei",
+"razor rusher",
+"timing boi",
+"wolf rusher",
+"defense breaker",
+"non standard moves",
+"crouch baby",
+"b24 stuff",
+"low low"
+];
+function rando(max){
+	return Math.floor(Math.random() * max);
+}
+
+function roll(){
+	var len = leistuff.length;
+	var first = leistuff[rando(len)];
+	do {
+		var second = leistuff[rando(len)];
+	} while(second == first);
+	do {
+		var third = leistuff[rando(len)];
+	} while(second == third);
+	document.getElementById("first").innerHTML = first;
+	document.getElementById("second").innerHTML = second;
+	document.getElementById("third").innerHTML = third;
+}
+
+roll();
